99e51ffc233c2d4099bd2233c2792c90abc7a357
Diego Guerra <dgoguerra.or@gmail.com>
2016-12-09 18:21:48 +0100

    added examples

---
+++ examples/example.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    spawn = require('child_process').spawn,
+    debugProc = require('../index.js')('proc');
+
+function spawnProcess() {
+    return spawn('./example-cmd.sh', []);
+}
+
+setTimeout(function() {
+    console.log('process 1');
+    var proc = spawnProcess();
+    debugProc(proc);
+}, 0);
+
+setTimeout(function() {
+    console.log('process 2 (only stderr)');
+    var proc = spawnProcess();
+    debugProc(proc, {stdout: false, decorate: 'name'});
+}, 500);
+
+setTimeout(function() {
+    console.log('process 3 (only stdout)');
+    var proc = spawnProcess();
+    debugProc(proc, {stderr: false, decorate: 'pid'});
+}, 1000);
+
+setTimeout(function() {
+    console.log('process 4');
+    var proc = spawnProcess();
+    debugProc(proc, {decorate: function(proc) {return 'proc4: ';}});
+}, 1500);
+
+setTimeout(function() {
+    console.log('process 5');
+    var proc = spawnProcess();
+    debugProc(proc, {decorate: function(proc) {return 'proc5: ';}});
+}, 2000);
